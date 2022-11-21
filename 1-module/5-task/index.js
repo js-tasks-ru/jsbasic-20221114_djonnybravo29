b1cf89c2ad9f0103c2aeddd6ab3ac6b39942853a
djonnybravo <djonnybravo29@gmail.com>
2022-11-21 21:29:58 +0300

    Task 5 module 1

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,12 @@
-function truncate(str, maxlength) {
-  // ваш код...
-}
+  function truncate(str, maxlength) {
+    if (str.length > maxlength) {
+      return str.slice(0, maxlength - 1) + "...";
+    }
+    return str
+  }
+  function truncate(str, maxlength) {
+    if (str.length > maxlength) {
+      return str.slice(0, maxlength - 1) + "…";
+    }
+    return str
+  }
