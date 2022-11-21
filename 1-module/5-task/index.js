6d7d132e2fca0a77517d5030a04783c253f2d196
djonnybravo <djonnybravo29@gmail.com>
2022-11-21 21:37:10 +0300

    taks 5 module 1

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,10 +1,4 @@
-  function truncate(str, maxlength) {
-    if (str.length > maxlength) {
-      return str.slice(0, maxlength - 1) + "...";
-    }
-    return str
-  }
-  function truncate(str, maxlength) {
+function truncate(str, maxlength) {
     if (str.length > maxlength) {
       return str.slice(0, maxlength - 1) + "…";
     }
