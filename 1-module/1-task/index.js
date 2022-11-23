3279faa66cb9011b30408e9bd08f39200c2bfe9f
djonnybravo <djonnybravo29@gmail.com>
2022-11-23 12:52:39 +0300

    calculate factorial func

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  if (n == 0) {
+    return 1;
+  } else if (n<0) {
+    return undefined;
+  } else if (n > 0) {
+     for(let i = n; --i;) {
+      n *= i;
+     }
+  }
+  return n;
 }
