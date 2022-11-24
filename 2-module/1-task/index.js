24b2b2e70d36ba8fd92b11e914c1ea9fafed6c8e
djonnybravo <djonnybravo29@gmail.com>
2022-11-24 23:38:01 +0300

    sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumOfSalaries = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === "number" && typeof salaries[key] != NaN && salaries[key] != Infinity && salaries[key] != -Infinity) {
+      sumOfSalaries += salaries[key]
+    }
+  }
+  return sumOfSalaries
 }
