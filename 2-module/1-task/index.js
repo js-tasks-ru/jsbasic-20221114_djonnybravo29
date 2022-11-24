8670ded682315c46d8453bc97e43325a0d50b36e
djonnybravo <djonnybravo29@gmail.com>
2022-11-24 23:40:58 +0300

    Необрабатывался NaN - поправил

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,7 @@
 function sumSalary(salaries) {
   let sumOfSalaries = 0;
   for (let key in salaries) {
-    if (typeof salaries[key] === "number" && typeof salaries[key] != NaN && salaries[key] != Infinity && salaries[key] != -Infinity) {
+    if (typeof salaries[key] === "number" && !isNaN(salaries[key]) && salaries[key] != Infinity && salaries[key] != -Infinity) {
       sumOfSalaries += salaries[key]
     }
   }
