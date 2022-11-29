4fd05c3186a5fb401ba27cd85fbceb713291e2dc
djonnybravo <djonnybravo29@gmail.com>
2022-11-29 18:18:58 +0300

    Внес правки

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -2,11 +2,8 @@ function getMinMax(str) {
   let numArr = str.split(' ')
                   .filter(item => isFinite(item))
                   .map(item => Number(item))
-  let result = {
-    min: 0,
-    max: 0,
-  }
-  result.min = Math.min.apply(null, numArr)
-  result.max = Math.max.apply(null, numArr)
-  return result
+  return {
+     min: Math.min(...numArr),
+     max: Math.max(...numArr),
+   }
 }
