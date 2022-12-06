9fd3d6bdbece2ccd90176dd36722f605c0c20533
djonnybravo <djonnybravo29@gmail.com>
2022-12-06 09:53:25 +0300

    убрал лишний console.log

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,8 +1,6 @@
 function hideSelf() {
   const hideSelfButton = document.querySelector('.hide-self-button');
-  console.log(hideSelfButton);
   hideSelfButton.addEventListener('click', () => {
     hideSelfButton.hidden = true;
-  }
-  )
+  })
 }
